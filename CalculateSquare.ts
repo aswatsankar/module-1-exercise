dff339e3f587c1a5fffb0ba8bb321b3f3ec5e58f
sankara.narayanan <sankara.narayanan@landservices.com.au>
2024-04-15 15:27:15 +0930

    First Commit

---
+++ CalculateSquare.ts
@@ -0,0 +1,7 @@
+//Create a function calculate Square that takes a number as input and returns its square 
+
+function calculateSquare(num: number): number {
+    return num * num;
+}
+const Squarevalue = calculateSquare(7);
+console.log(Squarevalue);
---
+++ CalculateTriangle.ts
@@ -0,0 +1,8 @@
+//Create an arrow function calculate Triangle Area that takes the base and height of a triangle as parametesr and returns its area
+const calculateTriangleArea = (B: number, H: number): number => {
+    return 0.5 * B * H;
+};
+
+
+const Trianglearea = calculateTriangleArea(5, 10);
+console.log(Trianglearea);
